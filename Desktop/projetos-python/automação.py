d1d20a2f12c941150f10b050bd81d23663338ada
j-ma-rcos <joaosantoslucas2013@hotmail.com>
2022-05-18 00:04:28 -0300

    projeto de automação + modificação no trynumer

--- Desktop/projetos-python/automação.py
+++ Desktop/projetos-python/automação.py
@@ -1,8 +1,35 @@
+
 import pyautogui;
 import time;
 import pyperclip;
 
+link = "https://www.nuinvest.com.br/"
+email = " "
+senha=" "
+
+pyautogui.PAUSE = 1;
 #1 passo abrir navegador na internet 
-#2 copiar link do drive inde ta a base de dados 
-#3 entra no google drive
-#4 abrir a pasta "aula1 " 
+pyautogui.press("winleft")
+pyautogui.write("edge")
+pyautogui.press("enter")
+
+#2 acessa o link
+pyperclip.copy(link)
+pyautogui.hotkey("ctrl" , "v")
+pyautogui.press("enter")
+time.sleep(5)
+
+#clica no login
+pyautogui.click(x=2671, y=101);
+time.sleep(5)
+#clica onde vai por email
+pyautogui.click(x=1944, y=322);
+pyperclip.copy(email)
+pyautogui.hotkey("ctrl","v")
+pyautogui.click(x=1825, y=360)
+#clica onde vai por a senha
+pyautogui.click(x=1896, y=386)
+pyperclip.copy(senha)
+pyautogui.hotkey("ctrl", "v")
+#clica no botão de logar
+pyautogui.click(x=1919, y=512)
--- Desktop/projetos-python/trynumber.py
+++ Desktop/projetos-python/trynumber.py
@@ -4,7 +4,7 @@ from random import randint
 
 print( " bem vindo ao TENTE ACERTAR O NUMERO ")
 
-V1 = randint(1,4) 
+V1 = randint(1,10) 
 
 V2 = input(str("deseja jogar? [y/n]\n"))
 
@@ -12,6 +12,6 @@ if V2 == 'y' : v3 = int(input('digite o numero que vc acha que foi sorteado\n'))
 elif V2 == 'n' : quit()
 else: print(" resposta invalida ")
 
-while(v3 != V1) : v3 = int(input(' voce errou, tente novamente '))
+while(v3 != V1) : v3 = int(input(' voce errou, tente novamente \n'))
 
 if v3 == V1 : print(' voce acertou no numero era : ' , V1)
